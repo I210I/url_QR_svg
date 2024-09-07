d58ee9dc6057c523ebf0466411a89a50b979f72b
210 <scopty210@gmail.com>
2024-09-07 08:34:23 -0600

    210: cambio paraencriptar una url y suburl

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,87 +2,95 @@
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
-using System.Windows.Input;
-using static System.Net.Mime.MediaTypeNames;
-namespace QR_URL_GENERATOR_SVG;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace QR_URL_GENERATOR_SVG
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
 
-    public bool valid_url(string url) => Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult);
 
-    private void generar()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        var generar = QR_SVG_Generator.Instance;
-        string url = tb_url.Text;
-        if (!string.IsNullOrEmpty(url) && valid_url(url))
+        public MainWindow()
         {
-            generar.generate_QR(url);
-            tb_url.Text = "";
+            InitializeComponent();
         }
-    }
-    private void encriptar()
-    {
-        try
+
+        public bool valid_url(string url) => Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult);
+
+        private void generar()
         {
-            string keyString = "mi_clavemi_clave"; // Debe ser de 16, 24, o 32 caracteres
-            string url = tb_url.Text;
-
-            if (string.IsNullOrEmpty(url))
+            var generar = QR_SVG_Generator.Instance;
+            string url = tb_urlbase + tb_url.Text;
+            if (!string.IsNullOrEmpty(url) && valid_url(url))
             {
-                MessageBox.Show("La URL no puede estar vacía.");
-                return;
+                generar.generate_QR(url);
+                tb_url.Text = "";
             }
-
-            var key = Encoding.UTF8.GetBytes(keyString);
-            string encryptedBase64;
-
-            using (var aesAlg = Aes.Create())
+        }
+        private static string encriptar(string url)
+        {
+            tb_urlcodificada.Text = string.Empty;
+            try
             {
-                aesAlg.Key = key;
-                aesAlg.Mode = CipherMode.ECB; // Modo ECB no requiere IV
-                aesAlg.Padding = PaddingMode.PKCS7;
-                MemoryStream msEncrypt;
-                using (var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, null))
-                using ( msEncrypt = new System.IO.MemoryStream())
-                using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                string keyString = "pmYyzUrq7U6oCZnHrqaEa2Yz1s8A3ydQ"; // Debe ser de 16, 24, o 32 caracteres
+
+
+                if (string.IsNullOrEmpty(url))
                 {
-                    swEncrypt.Write(url);
+                    MessageBox.Show("La URL no puede estar vacía.");
+                    return "";
                 }
 
-                encryptedBase64 = Convert.ToBase64String(msEncrypt.ToArray());
+                var key = Encoding.UTF8.GetBytes(keyString);
+                string encryptedBase64 = "";
+
+                using (var aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = key;
+                    aesAlg.Mode = CipherMode.ECB; // Modo ECB no requiere IV
+                    aesAlg.Padding = PaddingMode.PKCS7;
+                    MemoryStream msEncrypt;
+                    using (var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, null))
+                    using (msEncrypt = new System.IO.MemoryStream())
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(url);
+                    }
+
+                    encryptedBase64 = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+                return encryptedBase64;
             }
-
-            tb_urlcodificada.Text = encryptedBase64;
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            return "";
         }
-        catch (Exception ex)
-        {
-            MessageBox.Show("Ocurrió un error: " + ex.Message);
-        }
-    }
-    private void event_generar(object sender, RoutedEventArgs e)
-    {
-        generar();
-    }
-
-    private void event_generar(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter)
+        private void event_generar(object sender, RoutedEventArgs e)
         {
             generar();
         }
-    }
 
-    private void event_encriptar(object sender, RoutedEventArgs e)
-    {
-        encriptar();
+
+
+        private void event_encriptar(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_vw.Text) || string.IsNullOrEmpty(tb_dl.Text))
+                return;
+            string json = "{\"_vw\":\"" + encriptar(tb_vw.Text) + "\",\"_dl\":\"" + encriptar(tb_dl.Text) + "\",\"_t\":\"\"}";
+
+            tb_vw.Text = "";
+            tb_dl.Text = "";
+            tb_urlcodificada.Text = json;
+
+        }
+
+        private void limpiar(object sender, RoutedEventArgs e)
+        {
+            tb_urlcodificada.Text = "";
+        }
     }
 }
